82e9b3d99e680968ed517bd94e42226e37469047
“Kyle <kyle.moodley@bbd.co.za>
2024-02-09 10:30:42 +0200

    int updated

--- UsersTable.sql
+++ UsersTable.sql
@@ -2,7 +2,7 @@ CREATE TABLE Users(
     [UserID][int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
     [FirstName][varchar](120),
     [LastName][varchar](120),
-    [RoleID][int](120),
+    [RoleID][int],
     [ContactID][int],
     [Username][varchar](120) NOT NULL,
     [Password][varchar](120) NOT NULL
