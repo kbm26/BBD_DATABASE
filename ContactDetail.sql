bdb6e29fad070d6a389377caad24a24db69cd9c9
Vuyo Mathe <88540323+DarthNola@users.noreply.github.com>
2024-02-09 09:09:19 +0200

    Table creation for contact details , fund , role , status tales

---
+++ ContactDetail.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[ContactDetail](
+	[ContactID] [int]  Primary Key IDENTITY(1,1) NOT NULL ,
+	[Email] [varchar],
+	[PhoneNumber] [int](13) NOT NULL,
+	
+)
---
+++ Fund.sql
@@ -0,0 +1,4 @@
+CREATE TABLE [dbo].[Fund](
+	[FundsID] [int]  Primary Key IDENTITY(1,1) NOT NULL ,
+	[Budget] [money],
+)
