bd29b108b2705bd6ebd57bbe23bb0c0c246f4ea7
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-09 13:10:30 +0200

    create table

--- UsersTable.sql
+++ UsersTable.sql
@@ -1,9 +1,8 @@
-CREATE TABLE Users(
-    [UserID][int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
-    [FirstName][varchar](120),
-    [LastName][varchar](120),
-    [RoleID][int],
-    [ContactID][int],
-    [Username][varchar](120) NOT NULL,
-    [Password][varchar](120) NOT NULL
+CREATE TABLE User(
+    [UserID] [int] IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+    [FirstName] [varchar](120),
+    [LastName] [varchar](120),
+    [Username] [varchar](120) NOT NULL,
+    [Password] [varchar](120) NOT NULL [RoleID] [int],
+    [ContactID] [int],
 );
