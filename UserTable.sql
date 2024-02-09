8645544a1e453f604ba5d7da092605734ce6f2fc
“Kyle <kyle.moodley@bbd.co.za>
2024-02-09 10:22:23 +0200

    Identity updated on userID

--- UserTable.sql
+++ UserTable.sql
@@ -1,5 +1,5 @@
 CREATE TABLE User(
-    [UserID][int] PRIMARY KEY NOT NULL,
+    [UserID][int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
     [FirstName][varchar](120),
     [LastName][varchar](120),
     [Role][varchar](120),
