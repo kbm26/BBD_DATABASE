0dd6eaace59ad2a06b3c4da8c653dba1a76da556
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-09 05:24:40 +0200

    create table

---
+++ DocumentTable.sql
@@ -0,0 +1,7 @@
+use BBD;
+
+GO
+    CREATE TABLE Document(
+        [DocumentDescription] [varchar](255) DEFAULT "not available",
+        [DocumentFile] [varbinary](max) DEFAULT "not available",
+    );
