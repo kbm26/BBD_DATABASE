232b540c6a0f44521e5cdd83ec92985a1a4b4af4
Alfred Malope <Alfred.Malope@bbd.co.za>
2024-02-09 13:20:13 +0200

    add comments

--- CreateTable/StudentAppliction.sql
+++ CreateTable/StudentAppliction.sql
@@ -3,10 +3,10 @@ CREATE TABLE [dbo].[StudentApplication] (
     [ApplicationID] [int] IDENTITY(1, 1) PRIMARY KEY NOT NULL,
     [Grade] [int] NOT NULL,
     [Amount] [money],
-    [ApplicationStatus] [int] NOT NULL DEFAULT '3',
     --1. Approved 2. Rejected 3. Pending
+    [ApplicationStatus] [int] NOT NULL DEFAULT '3', 
+    --Comment for rejection   
     [Comment] [varchar](255) NULL,
-    --Comment for rejection
     [StudentID] [int] NOT NULL,
     [DocumentID] [int] NOT NULL,
 );
