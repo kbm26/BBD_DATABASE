f3d3ef14df5397166d6b5927a6fb36ae9b1140ea
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-09 06:35:00 +0200

    add nulls

--- DocumentTable.sql
+++ DocumentTable.sql
@@ -2,6 +2,17 @@ use BBD;
 
 GO
     CREATE TABLE Document(
-        [DocumentDescription] [varchar](255) DEFAULT "not available",
-        [DocumentFile] [varbinary](max) DEFAULT "not available",
+        [DocumentDescription] [varchar](255),
+        [DocumentFile] [varbinary](max),
     );
+
+GO
+    --constraints
+    ----add NULL Document
+ALTER TABLE
+    Document ALTER DocumentDescription [varchar](255) NULL;
+
+GO
+    ----add NULL DocumentFile
+ALTER TABLE
+    Document ALTER DocumentFile [varbinary](max) NULL;
