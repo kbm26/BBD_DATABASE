e9bf380998ac64e123009f79e4aa98197a8f60d0
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-09 13:17:17 +0200

    move create table scripts

--- ContactDetail.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE [dbo].[ContactDetail](
-	[ContactID] [int] Primary Key IDENTITY(1, 1) NOT NULL,
-	[Email] [varchar](120) NOT NULL,
-	[PhoneNumber] [varchar](13) NOT NULL,
-)
--- UniversityTable.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE University(
-    [UniversityID] [int] IDENTITY(1, 1) PRIMARY KEY NOT NULL,
-    [Name] [varchar](120),
-    [Budget] [money],
-    [Province] [varchar](120),
-    [Comment] [varchar](250),
-    [StatusID] [int],
-    [UserID][int] 
-);
