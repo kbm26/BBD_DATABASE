9e73d4b97b5d10bf76139a6550149b9f16be16eb
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-09 13:11:25 +0200

    create table

--- Student.sql
+++ Student.sql
@@ -1,15 +1,20 @@
 --Creating Students table
-CREATE TABLE [dbo].[Students]
-(
-    [StudentID] [INT] IDENTITY(1,1) PRIMARY KEY NOT NULL,
-    [UniversityID] [INT] NOT NULL,
-    [FirstName] [VARCHAR](120) NOT NULL,
-    [LastName] [VARCHAR](120)NOT NULL,
-    [IDNumber] [VARCHAR](13) NOT NULL,
+CREATE TABLE [dbo].[Students] (
+    [StudentID] [int] IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+    [FirstName] [varchar](120) NOT NULL,
+    [LastName] [varchar](120) NOT NULL,
+    [IDNumber] [varchar](13) NOT NULL,
     [BirthDate] [DATE] NOT NULL,
-    [Age] [INT],
-    [Gender] [VARCHAR](120)NOT NULL,
-    [Nationality] [VARCHAR](120)NOT NULL,
-    [RaceID] [INT] NOT NULL,
-    [ContactID] [INT] NOT NULL
-);GO
+    [Age] [int],
+    [Gender] [varchar](120) NOT NULL,
+    [Nationality] [varchar](120) NOT NULL,
+    [UniversityID] [int] NOT NULL,
+    [RaceID] [int] NOT NULL,
+    [ContactID] [int] NOT NULL
+);
+
+GO
+ALTER TABLE
+    Students
+ADD
+    CONSTRAINT ForeignKey
