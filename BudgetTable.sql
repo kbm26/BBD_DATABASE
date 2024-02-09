467282bb0d9b0f9ed99a8a68591b07db82fb4575
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-09 05:48:32 +0200

    add default money constraint

--- BudgetTable.sql
+++ BudgetTable.sql
@@ -1,4 +1,15 @@
 use BBD;
 
 GO
+    --create table
     CREATE TABLE BBDFund([Budget] [money], [Date] [date],);
+
+GO
+    --constraints
+    ----add default of zero money for Budget
+ALTER TABLE
+    BBDFund
+ADD
+    CONSTRAINT InitializeBudget DEFAULT 0.00 FOR Budget;
+
+GO
