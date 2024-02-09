6b3bd59289bcb17486794fd9051b5c8a9b61b37a
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-09 13:13:26 +0200

    create table

--- Role.sql
+++ Role.sql
@@ -1,4 +1,4 @@
 CREATE TABLE [dbo].[Role](
-	[RoleID] [int]  Primary Key IDENTITY(1,1) NOT NULL ,
-	[RoleType] [varchar](120) ,
+	[RoleID] [int] Primary Key IDENTITY(1, 1) NOT NULL,
+	[RoleType] [varchar](120),
 )
