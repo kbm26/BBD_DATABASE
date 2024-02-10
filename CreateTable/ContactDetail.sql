0a3cab2a32a35b63892ddf0ada3ba568b82aa0f0
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-10 20:04:24 +0200

    fix spelling

--- CreateTable/ContactDetail.sql
+++ CreateTable/ContactDetail.sql
@@ -1,4 +1,4 @@
-CREATE TABLE [dbo].[ContactDetaisl](
+CREATE TABLE [dbo].[ContactDetails](
 	[ContactID] [int] Primary Key IDENTITY(1, 1) NOT NULL,
 	[Email] [varchar](120) NOT NULL,
 	[PhoneNumber] [varchar](13) NOT NULL,
