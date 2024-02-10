e8cb5ecb6406362d285522c203b030752cf64218
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-10 20:18:44 +0200

    fix table nae mistake

--- Constraint/StudentApplicationConstraint.sql
+++ Constraint/StudentApplicationConstraint.sql
@@ -1,5 +1,5 @@
 ALTER TABLE
     [dbo].[StudentApplication]
 ADD
-    CONSTRAINT ForeignKeyStudent FOREIGN KEY (StudentID) REFERENCES [dbo].[Student](StudentID),
+    CONSTRAINT ForeignKeyStudent FOREIGN KEY (StudentID) REFERENCES [dbo].[Students](StudentID),
     CONSTRAINT ForeignKeyDocument FOREIGN KEY (DocumentID) REFERENCES [dbo].[Document](DocumentID)
