725cb629a75918e1924e2fc273e6d1e9502127f8
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-10 19:50:08 +0200

    format document

--- CreateTable/UniversityTable.sql
+++ CreateTable/UniversityTable.sql
@@ -2,7 +2,7 @@ CREATE TABLE [dbo].[University](
     [UniversityID] [int] IDENTITY(1, 1) PRIMARY KEY NOT NULL,
     [Name] [varchar](120),
     [Comment] [varchar](250),
-    [UserID][int] ,
+    [UserID] [int],
     [ProvinceID] [int],
     [StatusID] [int]
 );
