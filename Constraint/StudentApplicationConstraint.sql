47efc234252d192099cc77802565cf4525e779f7
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-10 20:01:55 +0200

    remove extra syntax error

--- Constraint/StudentApplicationConstraint.sql
+++ Constraint/StudentApplicationConstraint.sql
@@ -2,4 +2,4 @@ ALTER TABLE
     [dbo].[StudentApplication]
 ADD
     CONSTRAINT ForeignKeyStudent FOREIGN KEY (StudentID) REFERENCES [dbo].[Student](StudentID),
-    CONSTRAINT ForeignKeyDocument FOREIGN KEY (DocumentID) REFERENCES [dbo].[Document](DocumentID),
+    CONSTRAINT ForeignKeyDocument FOREIGN KEY (DocumentID) REFERENCES [dbo].[Document](DocumentID)
