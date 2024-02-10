f409d55e4de2dfd9c36ac498133386779fe165d5
Alfred Malope <Alfred.Malope@bbd.co.za>
2024-02-10 22:10:42 +0200

    update Univerdity table

--- CreateTable/UniversityTable.sql
+++ CreateTable/UniversityTable.sql
@@ -1,8 +1,5 @@
 CREATE TABLE [dbo].[University](
     [UniversityID] [int] IDENTITY(1, 1) PRIMARY KEY NOT NULL,
     [Name] [varchar](120),
-    [Comment] [varchar](250),
-    [UserID][int] ,
-    [ProvinceID] [int],
-    [StatusID] [int]
+    [ProvinceID] [int]
 );
