919d87764a8e9859558577e2d182cc1de89abf3b
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-10 18:01:01 +0200

    order table fk

--- CreateTable/BudgetTable.sql
+++ CreateTable/BudgetTable.sql
@@ -1,25 +1,25 @@
 use BBD;
 
 --create table
-CREATE TABLE BBDFund(
+CREATE TABLE [dbo].[BBDFund](
     [FundID] [int] IDENTITY(1, 1) PRIMARY KEY NOT NULL,
-    [UniversityID] [int],
     [Budget] [money],
     [FinancialYearStart] [date],
+    [UniversityID] [int],
 );
 
 GO
     --constraints
     ----add default of zero money for Budget
 ALTER TABLE
-    BBDFund
+    [dbo].[BBDFund]
 ADD
     CONSTRAINT InitializeBBDFundBudget DEFAULT 0.00 FOR Budget;
 
 GO
     ----add default financial calendar year (From 1st march) for Date
 ALTER TABLE
-    BBDFund
+    [dbo].[BBDFund]
 ADD
     CONSTRAINT InitializeBBDFundBudgetDate DEFAULT DATEFROMPARTS (YEAR(GETDATE()), 3, 1) FOR FinancialYearStart;
 
