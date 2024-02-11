dac392eaf5d33ba618ab46caff57ca385913051d
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-11 21:23:31 +0200

    add ContactDetails from contact

--- Constraint/UsersTableConstraint.sql
+++ Constraint/UsersTableConstraint.sql
@@ -2,4 +2,4 @@ ALTER TABLE
     User
 ADD
     CONSTRAINT ForeignKeyRole FOREIGN KEY (RoleID) REFERENCES [dbo].[Role](RoleID),
-    CONSTRAINT ForeignKeyContact FOREIGN KEY (ContactID) REFERENCES [dbo].[Contact](ContactID),
+    CONSTRAINT ForeignKeyContact FOREIGN KEY (ContactID) REFERENCES [dbo].[ContactDetails](ContactID),
