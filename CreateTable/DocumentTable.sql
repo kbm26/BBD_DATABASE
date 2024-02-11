3deee7ce8dd951f6e1a1b4a0abda2a47ea5a1426
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-11 19:39:23 +0200

    add ApplicationID field

--- CreateTable/DocumentTable.sql
+++ CreateTable/DocumentTable.sql
@@ -5,6 +5,7 @@ GO
         [DocumentID] [int] IDENTITY(1, 1) PRIMARY KEY NOT NULL,
         [Transcript] [varbinary](max) NULL,
         [IdentityDocument] [varbinary](max) NULL,
+        [ApplicationID] [int],
     );
 
 GO
