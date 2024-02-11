9dcd191dd461dfcbe54269622cbcad8f91766423
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-11 08:15:44 +0200

    fix file name spelling mistake

--- Constraint/StudentInformationContaint.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE
-    [dbo].[StudentInformation]
-ADD
-    CONSTRAINT ForeignKeyUniversity FOREIGN KEY (UniversityID) REFERENCES [dbo].[University](UniversityID),
-    CONSTRAINT ForeignKeyRaceID FOREIGN KEY (RaceID) REFERENCES [dbo].[Race](RaceID),
-    CONSTRAINT ForeignKeyContactID FOREIGN KEY (ContactID) REFERENCES [dbo].[Contact](ContactID)
