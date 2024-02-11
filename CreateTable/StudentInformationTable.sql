579919a02563147c79fbb0c36723dcb0d4885151
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-11 08:23:29 +0200

    fix file name spelling mistake

---
+++ CreateTable/StudentInformationTable.sql
@@ -0,0 +1,10 @@
+--Creating Students table
+CREATE TABLE [dbo].[StudentInformation] (
+    [StudentID] [int] IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+    [IDNumber] [varchar](13) NOT NULL,
+    [BirthDate] [DATE] NOT NULL,
+    [Age] [smallint],
+    [Gender] [varchar](120) NOT NULL,
+    [UserID] [int] NOT NULL,
+    [RaceID] [int] NOT NULL,
+);
