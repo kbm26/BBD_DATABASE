7a1465a141e19dfcff5643fc9dd84477580d3926
Vuyo Mathe <88540323+DarthNola@users.noreply.github.com>
2024-02-11 21:44:32 +0200

    fixed a contraint

--- Constraint/StudentApplicationConstraint.sql
+++ Constraint/StudentApplicationConstraint.sql
@@ -1,7 +1,7 @@
 ALTER TABLE
     [dbo].[StudentApplication]
 ADD
-    CONSTRAINT ForeignKeyUniversity FOREIGN KEY (UniversityID) REFERENCES [dbo].[UniversityID](UniversityID),
+    CONSTRAINT ForeignKeyUniversity FOREIGN KEY (UniversityID) REFERENCES [dbo].[University](UniversityID),
     CONSTRAINT ForeignKeyStatus FOREIGN KEY (StatusID) REFERENCES  [dbo].[Status](StatusID);
 
 GO
--- Constraint/UniversityFundApplicationContraint.sql
+++ Constraint/UniversityFundApplicationContraint.sql
@@ -1,7 +1,7 @@
 ALTER TABLE
     [dbo].[UniversityFundApplication]
 ADD
-    CONSTRAINT ForeignKeyUniversityID FOREIGN KEY (UniversityID) REFERENCES [dbo].[UniversityID](UniversityID),
+    CONSTRAINT ForeignKeyUniversityID FOREIGN KEY (UniversityID) REFERENCES [dbo].[University](UniversityID),
     CONSTRAINT ForeignKeyUserID FOREIGN KEY (UserID) REFERENCES [dbo].[User](UserID),
     CONSTRAINT ForeignKeyStatusID FOREIGN KEY (StatusID) REFERENCES  [dbo].[Status](StatusID);
 
