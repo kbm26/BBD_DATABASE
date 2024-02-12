474cbc4a124adf5b418e18fec5018e8f0e9881fa
“Kyle <kyle.moodley@bbd.co.za>
2024-02-12 10:29:57 +0200

    pipeline done

--- PathGenerator.ps1
+++ PathGenerator.ps1
@@ -1,29 +1,28 @@
-# Define an array of folder paths
 $folderPaths = @(
     ".\CreateTable",
     ".\Constraint",
-    ".\InsertToTable"
+    ".\InsertToTable",
+    ".\Procedure",
+    ".\Functions",
+    ".\Views"
+  
 )
 
 
-# Define the output SQL file
 $outputSqlFile = "Paths.sql"
 
-# Open or create the output SQL file
+$bytes = [System.Text.Encoding]::UTF8.GetBytes("USE BBD"+"`n"+"
+EXECUTE dbo.DeleteAllTables" + "`n")
+$outputFile.Write($bytes, 0, $bytes.Length)
+
 $outputFile = [System.IO.File]::Open($outputSqlFile, [System.IO.FileMode]::Create)
-# Iterate over each folder path
 foreach ($folderPath in $folderPaths) {
-    # Check if the folder exists
     if (Test-Path $folderPath) {
-        # Get all files within the folder
         $files = Get-ChildItem -Path $folderPath -File
         
-        # Iterate over each file
         foreach ($file in $files) {
-            # Generate SQL INSERT statement for the file path
             $sqlStatement = ":r $($file.FullName)"
             
-            # Convert the SQL statement to bytes and write it to the output SQL file
             $bytes = [System.Text.Encoding]::UTF8.GetBytes($sqlStatement + "`n")
             $outputFile.Write($bytes, 0, $bytes.Length)
         }
@@ -32,7 +31,6 @@ foreach ($folderPath in $folderPaths) {
     }
 }
 
-# Close the output SQL file
 $outputFile.Close()
 
 Write-Output "File paths written to $outputSqlFile"
