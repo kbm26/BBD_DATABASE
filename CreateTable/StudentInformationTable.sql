206d37b58cbdf6e0aa03d404a5da6e172f88ca9f
alfred-malope <malopealfred12@gmail.com>
2024-02-12 08:30:16 +0200

    updating Age column

--- CreateTable/StudentInformationTable.sql
+++ CreateTable/StudentInformationTable.sql
@@ -3,7 +3,7 @@ CREATE TABLE [dbo].[StudentInformation] (
     [StudentID] [int] IDENTITY(1, 1) PRIMARY KEY NOT NULL,
     [IDNumber] [varchar](13) NOT NULL,
     [BirthDate] [DATE] NOT NULL,
-    [Age] [smallint],
+    [Age] AS (DATEDIFF(YEAR, BirthDate, GETDATE())) PERSISTED,
     [Gender] [varchar](120) NOT NULL,
     [UserID] [int] NOT NULL,
     [RaceID] [int] NOT NULL,
