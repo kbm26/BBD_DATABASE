099b6fe9dbb0a36bbbc9e989a171e43aaecfdd82
Vuyo Mathe <88540323+DarthNola@users.noreply.github.com>
2024-02-12 09:11:23 +0200

    Ordering of inserts

--- CreateTable/StudentInformationTable.sql
+++ CreateTable/StudentInformationTable.sql
@@ -3,7 +3,7 @@ CREATE TABLE [dbo].[StudentInformation] (
     [StudentID] [int] IDENTITY(1, 1) PRIMARY KEY NOT NULL,
     [IDNumber] [varchar](13) NOT NULL,
     [BirthDate] [DATE] NOT NULL,
-    [Age] AS (DATEDIFF(YEAR, BirthDate, GETDATE())) PERSISTED,
+    [Age] AS (CONVERT(int, DATEDIFF(DAY, [BirthDate], GETDATE()) / 365.25)) PERSISTED,
     [Gender] [varchar](120) NOT NULL,
     [UserID] [int] NOT NULL,
     [RaceID] [int] NOT NULL,
