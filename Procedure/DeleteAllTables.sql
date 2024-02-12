20728e328d683d8def683916756f1870849f19cc
alfred-malope <malopealfred12@gmail.com>
2024-02-12 10:23:30 +0200

    delete all tables

--- Procedure/DeleteAllTables.sql
+++ Procedure/DeleteAllTables.sql
@@ -1,31 +1,31 @@
 CREATE PROCEDURE DeleteAllTables
 AS
 BEGIN
-	ALTER TABLE [dbo].[StudentApplication] DROP CONSTRAINT [ForeignKeyStatus];
-	ALTER TABLE [dbo].[StudentInformation] DROP CONSTRAINT [ForeignKeyRaceID];
-	ALTER TABLE [dbo].[University] DROP CONSTRAINT [ForeignKeyProvince];
-	ALTER TABLE [dbo].[UniversityStudentInformation] DROP CONSTRAINT [ForeignKeyUniversity];
-	ALTER TABLE [dbo].[UniversityStudentInformation] DROP CONSTRAINT [ForeignKeyStudent];
-	ALTER TABLE [dbo].[StudentInformationStudentApplication] DROP CONSTRAINT [ForeignKey_StudentID];
-	ALTER TABLE [dbo].[StudentInformationStudentApplication] DROP CONSTRAINT [ForeignKey_ApplicationID];
-	ALTER TABLE [dbo].[Document] DROP CONSTRAINT [ForeignKeyStudentApplication];
-	ALTER TABLE [dbo].[User] DROP CONSTRAINT [ForeignKeyContact];
-     DROP TABLE [dbo].[BBDFund];
-	 DROP TABLE [dbo].[ContactDetails];
-	 DROP TABLE [dbo].[Document];
-	 DROP TABLE [dbo].[Province];
-	 DROP TABLE [dbo].[Race];
-	 DROP TABLE [dbo].[Role];
-	 DROP TABLE [dbo].[Status];
-	 DROP TABLE [dbo].[StudentApplication];
-	 DROP TABLE [dbo].[StudentInformation];
-	 DROP TABLE [dbo].[StudentInformationStudentApplication];
-	 DROP TABLE [dbo].[University];
-	 DROP TABLE [dbo].[UniversityFundApplication];
-	 DROP TABLE [dbo].[UniversityStudentInformation];
-	 DROP TABLE [dbo].[UniversityUser];
-	 DROP TABLE [dbo].[User];
-	 DROP TABLE [dbo].[UserRole];
+    ALTER TABLE [dbo].[StudentApplication] DROP CONSTRAINT [ForeignKeyStatus];
+    ALTER TABLE [dbo].[StudentInformation] DROP CONSTRAINT [ForeignKeyRaceID];
+    ALTER TABLE [dbo].[University] DROP CONSTRAINT [ForeignKeyProvince];
+    ALTER TABLE [dbo].[UniversityStudentInformation] DROP CONSTRAINT [ForeignKeyUniversity];
+    ALTER TABLE [dbo].[UniversityStudentInformation] DROP CONSTRAINT [ForeignKeyStudent];
+    ALTER TABLE [dbo].[StudentInformationStudentApplication] DROP CONSTRAINT [ForeignKey_StudentID];
+    ALTER TABLE [dbo].[StudentInformationStudentApplication] DROP CONSTRAINT [ForeignKey_ApplicationID];
+    ALTER TABLE [dbo].[Document] DROP CONSTRAINT [ForeignKeyStudentApplication];
+    ALTER TABLE [dbo].[User] DROP CONSTRAINT [ForeignKeyContact];
+    DROP TABLE [dbo].[BBDFund];
+    DROP TABLE [dbo].[ContactDetails];
+    DROP TABLE [dbo].[Document];
+    DROP TABLE [dbo].[Province];
+    DROP TABLE [dbo].[Race];
+    DROP TABLE [dbo].[Role];
+    DROP TABLE [dbo].[Status];
+    DROP TABLE [dbo].[StudentApplication];
+    DROP TABLE [dbo].[StudentInformation];
+    DROP TABLE [dbo].[StudentInformationStudentApplication];
+    DROP TABLE [dbo].[University];
+    DROP TABLE [dbo].[UniversityFundApplication];
+    DROP TABLE [dbo].[UniversityStudentInformation];
+    DROP TABLE [dbo].[UniversityUser];
+    DROP TABLE [dbo].[User];
+    DROP TABLE [dbo].[UserRole];
 END
 
 EXEC DeleteAllTables;
